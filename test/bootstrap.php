c11922ce12206a8f6a43636cd4817251239da007
Dmitriy Tyurin <fobia3d@gmail.com>
2014-02-07 20:37:38 +0400

    add tests

---
+++ test/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Dmitriy Tyurin <fobia3d@gmail.com>
+ */
+
+$autoloadFile = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($autoloadFile)) {
+    throw new RuntimeException('Install dependencies to run phpunit.');
+}
+require_once $autoloadFile;
+
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->add('', 'test');
+$loader->register();
